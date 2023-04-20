b22e028f28a80bdd90088e2109f6ea1524b61ee9
Scott Green <s_g214@student.usc.edu.au>
2023-04-20 17:54:02 +1000

    trapezoids calculator functionality, calculated area formatting and webpage favicon - Scott.

--- Exercise 1 - Area Calculators/scripts/calculator.js
+++ Exercise 1 - Area Calculators/scripts/calculator.js
@@ -1,10 +1,15 @@
 // // // -----  Class methods  -----   //  //  //
 
+//  -----   Function to round a float and format to a given number of decimals  -----   //
+function formatFloat(value, decimals){
+
+    //  Round the given value to the number of decimal places given and convert it to a float to remove trailing zeros
+    return parseFloat(Number(Math.round(value + 'e' + decimals) + 'e-' + decimals).toFixed(decimals));
+
+}
+
 //  -----  Function to calculate the area of a given shape -----   //
 function calculateArea(_shape){
-
-    //  Empty the answer field so that any previous answers are hidden
-    //emptyAnswer();
 
     //  Show the calculator required based on the string passed in    
     switch (_shape) {
@@ -12,7 +17,7 @@ function calculateArea(_shape){
         case 'square':  
 
             //  Get value of side of square from form
-            a = document.getElementById("squareInput").value;
+            a = parseFloat(document.getElementById("squareInput").value);
 
             //  Calculate area of square
             areaCalculated = a * a;
@@ -22,7 +27,7 @@ function calculateArea(_shape){
         case 'circle':
 
             //  Get value of radius of circle from form
-            r = document.getElementById("circleInput").value;
+            r = parseFloat(document.getElementById("circleInput").value);
 
             //  Calculate area of circle
             areaCalculated = Math.PI * (r * r);
@@ -31,7 +36,19 @@ function calculateArea(_shape){
 
         case 'trapezoid':
 
-            areaCalculated = "Area of Trapezoid";
+            // Get value of base a of trapezoid from form
+            a = parseFloat(document.getElementById("trapezoidInputA").value);
+
+            // Get value of base b of trapezoid from form
+            b = parseFloat(document.getElementById("trapezoidInputB").value);        
+
+            // Get height of trapezoid from form
+            h = parseFloat(document.getElementById("trapezoidInputH").value);
+        
+            //  Calculate area of trapezoid
+            areaCalculated = a + b;
+            areaCalculated = areaCalculated / 2;
+            areaCalculated = areaCalculated * h;
             
             break;
     
@@ -42,7 +59,7 @@ function calculateArea(_shape){
     }
 
     //  Show the calculated area
-    alert(areaCalculated);
+    alert(formatFloat(areaCalculated, 2));
 
     //  Destroy the variable which stores the calculated area
     delete areaCalculated;
